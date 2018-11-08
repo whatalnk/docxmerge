7dacb615aadc4f42d26d9c0fca5d6229cb1f3a29
Wataru NAKAI <watnk0806@gmail.com>
2018-11-08 19:25:16 +0900

    Init

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace docxmerge
+{
+  class Program
+  {
+    public static int Main(string[] args)
+    {
+      // https://docs.microsoft.com/ja-jp/dotnet/api/documentformat.openxml.wordprocessing.altchunk?view=openxml-2.8.1
+
+      var app = new CommandLineApplication(throwOnUnexpectedArg: false);
+      app.Name = nameof(docxmerge);
+      app.Description = "Mearge .docx files. ";
+      app.HelpOption("-h|--help");
+      app.ExtendedHelpText = @"Merged files should be the end of argument like: 
+      docxmerge [options] file1 file2
+      ";
+      var optionTemplateFile = app.Option(
+        template: "-t|--template",
+        description: "Template filename. merged files will be appended after the contents of template file. ",
+        optionType: CommandOptionType.SingleValue
+      );
+
+      var optionOutFile = app.Option(
+        template: "-o|--output",
+        description: "Output filename",
+        optionType: CommandOptionType.SingleValue
+      );
+
+      app.OnExecute(() =>
+      {
+        // string mainfile = @"main.docx";
+        // string titlefile = @"Title.docx";
+        // var chapters = new List<string> { @"Chapter1.docx", @"Chapter2.docx", @"Chapter3.docx", @"References.docx" };
+        // Because position of last paragraph is not updated while looping
+        // chapters.Reverse();
+        var templateFile = optionTemplateFile.Value();
+        var outFile = optionOutFile.Value();
+        var mergedFiles = app.RemainingArguments;
+        mergedFiles.Reverse();
+        File.Delete(outFile);
+        File.Copy(templateFile, outFile);
+
+        using (WordprocessingDocument myDoc = WordprocessingDocument.Open(outFile, true))
+        {
+          MainDocumentPart mainPart = myDoc.MainDocumentPart;
+          foreach (var f in mergedFiles.Select((Value, Index) => new { Value, Index }))
+          {
+            string altChunkId = $"AltChunkId{f.Index}";
+            AlternativeFormatImportPart chunk = mainPart.AddAlternativeFormatImportPart(
+              AlternativeFormatImportPartType.WordprocessingML, altChunkId
+            );
+            using (FileStream fileStream = File.Open(f.Value, FileMode.Open))
+            {
+              chunk.FeedData(fileStream);
+            }
+            AltChunk altChunk = new AltChunk();
+            altChunk.Id = altChunkId;
+            mainPart.Document.Body.InsertAfter(altChunk, mainPart.Document.Body.Elements<Paragraph>().Last());
+
+          }
+          mainPart.Document.Save();
+        }
+      });
+      return app.Execute(args);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+## About
+
+Merge MS Word .docx files as `altChunk`s
+
+## Usage 
+
+```
+docxmerge.exe -t Title.docx -o main.docx Chapter1.docx Chapter2.docx Chapter3.docx References.docx
+```
+
+## Build executable
+
+```
+dotnet publish -c Release -r win-x64
+```
+
